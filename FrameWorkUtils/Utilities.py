937a542686c58533d85dc87c1b4bbce6f111b10d
parthap <prathap.ufttest@gmail.com>
2020-04-22 23:14:24 +0530

    added

--- FrameWorkUtils/Utilities.py
+++ FrameWorkUtils/Utilities.py
@@ -1,21 +1,23 @@
 from configparser import ConfigParser
 import pytest
 import allure
+import os
 
 class Utils():
 
     @pytest.fixture(scope='session')
     def pre_and_post_activities(self):
-        print("*****************************************************")
+        print("*****************Pre and POST******************")
         self.load_ConfigFile()
         yield
 
     @allure.step("Loading Config file")
     def load_ConfigFile(self):
-        print("*****************************************************")
+        print("Test file......"+str(os.path.exists("../Configuration/Environment.cnf")))
         global config
         config=ConfigParser()
         config.read("../Configuration/Environment.cnf")
+        print("**********Reading config file***************")
     @allure.step("Get Data from the section : {0} for the field : {1} from the config file")
     def getConfigData(self,section,property):
         print("*****************************************************")
