07a303d0e1117229428a1de0273d92bcc6cc2a5c
parthap <prathap.ufttest@gmail.com>
2020-04-22 23:22:00 +0530

    added

--- FrameWorkUtils/Utilities.py
+++ FrameWorkUtils/Utilities.py
@@ -13,10 +13,11 @@ class Utils():
 
     @allure.step("Loading Config file")
     def load_ConfigFile(self):
-        print("Test file......"+str(os.path.exists("../Configuration/Environment.cnf")))
+        cnfFilepath=str(os.getcwd())[0:str(os.getcwd()).rfind("\\")]+"\\Configuration\\Environment.cnf"
+        print("Test file......"+str(os.path.exists(cnfFilepath)))
         global config
         config=ConfigParser()
-        config.read("../Configuration/Environment.cnf")
+        config.read(cnfFilepath)
         print("**********Reading config file***************")
     @allure.step("Get Data from the section : {0} for the field : {1} from the config file")
     def getConfigData(self,section,property):
